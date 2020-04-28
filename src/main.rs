0b011cf054072014a0b198164937c1ba82ab072c
Jared Wasinger <j-wasinger@hotmail.com>
2020-04-28 13:57:36 -0700

    print elapsed time for pairing

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
+use std::time::{Instant};
+
 use eip1962::engines::bls12_381::*;
 use eip1962::weierstrass::Group;
 use eip1962::pairings::PairingEngine;
 use eip1962::traits::{FieldElement,ZeroAndOne};
 use eip1962::extension_towers::fp12_as_2_over3_over_2::{Fp12};
 
-fn wasmsnark_test_case() {
+fn bench_two_point_pairing() {
     let gt_one = Fp12::one(&BLS12_381_EXTENSION_12_FIELD);
 
     let mut p = BLS12_381_G1_GENERATOR.clone().mul(vec![27]);
@@ -18,31 +20,14 @@ fn wasmsnark_test_case() {
     p2.normalize();
     q2.normalize();
 
+    let start = Instant::now();
     let res = BLS12_381_PAIRING_ENGINE.pair(&[p.clone(), p2.clone()], &[q.clone(), q2.clone()]).unwrap();
+    let duration = start.elapsed();
+    println!("Time elapsed in bench() is: {:?}", duration);
 
     assert!(res == gt_one);
 }
 
-fn three_point_pairing() {
-    let p = BLS12_381_G1_GENERATOR.clone();
-    let q = BLS12_381_G2_GENERATOR.clone();
-
-    let mut p2 = p.mul(vec![12345678]);
-    p2.normalize();
-
-    let mut q2 = q.mul(vec![12345678]);
-    q2.normalize();
-
-    let ans1 = BLS12_381_PAIRING_ENGINE.pair(&[p.clone()], &[q2]).unwrap();
-    let ans2 = BLS12_381_PAIRING_ENGINE.pair(&[p2], &[q.clone()]).unwrap();
-    let ans3 = BLS12_381_PAIRING_ENGINE.pair(&[p], &[q]).unwrap();
-    let ans3 = ans3.pow(&vec![12345678]);
-
-    assert!(ans1 == ans2);
-    assert!(ans1 == ans3);
-}
-
 fn main() {
-    // three_point_pairing();
-    websnark_test_case();
+    bench_two_point_pairing();
 }
